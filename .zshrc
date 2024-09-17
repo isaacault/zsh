fd215843414a391dac08da63d1b2df3ed79c8694
Isaac Ault <isaac.ault@gmail.com>
2024-09-17 21:43:27 +0100

    Initial zsh configuration.

---
+++ .zshrc
@@ -0,0 +1,39 @@
+# Aliases
+source $ZDOTDIR/aliases
+
+
+# Zsh Options
+setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
+
+setopt AUTO_PUSHD           # Push the current directory visited on the stack.
+setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack.
+setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
+
+
+# Initialise completion
+source $ZDOTDIR/completion.zsh
+
+# Vim mode
+bindkey -v
+export KEYTIMEOUT=1
+
+autoload -Uz edit-command-line
+zle -N edit-command-line
+bindkey -M vicmd v edit-command-line
+
+alias vimdiff='nvim -d'
+alias vim='nvim'
+alias vi='nvim'
+
+# Hook direnv
+eval "$(direnv hook zsh)"
+
+# Set up fzf key bindings and fuzzy completion
+source <(fzf --zsh)
+
+# Syntax Highlighting
+source $ZDOTDIR/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# Starship prompt (https://starship.rs)
+eval "$(starship init zsh)"
+export STARSHIP_CONFIG=$ZDOTDIR/prompt/starship.toml
---
+++ completion.zsh
@@ -0,0 +1,49 @@
+# Load more completions
+fpath=($ZDOTDIR/plugins/zsh-completions/src $fpath)
+
+# Should be called before compinit
+zmodload zsh/complist
+
+# Use hjlk in menu selection (during completion)
+# Doesn't work well with interactive mode
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+
+# Escape menu on esc
+bindkey -M menuselect '^[' undo
+
+autoload -U compinit; compinit
+_comp_options+=(globdots) # With hidden files
+
+setopt MENU_COMPLETE        # Automatically highlight first element of completion menu
+setopt AUTO_LIST            # Automatically list choices on ambiguous completion.
+setopt COMPLETE_IN_WORD     # Complete from both ends of a word.
+
+# Allow you to select in a menu
+zstyle ':completion:*' menu select
+
+# Autocomplete options for cd instead of directory stack
+zstyle ':completion:*' complete-options true
+
+zstyle ':completion:*' file-sort modification
+
+zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
+zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %D %d --%f'
+zstyle ':completion:*:*:*:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:*:*:*:warnings' format ' %F{red}-- no matches found --%f'
+# zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+# Colors for files and directory
+zstyle ':completion:*:*:*:*:default' list-colors ${(s.:.)LS_COLORS}
+
+# Only display some tags for the command cd
+zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
+# zstyle ':completion:*:complete:git:argument-1:' tag-order !aliases
+
+# Required for completion to be in good groups (named after the tags)
+zstyle ':completion:*' group-name ''
+
+zstyle ':completion:*:*:-command-:*:*' group-order aliases builtins functions commands
+
+zstyle ':completion:*' keep-prefix true
